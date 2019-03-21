9eb3da964282a5adb6acf3fe02ab920d3d0ee51d
Patrick Rodacker <patrick@rodacker.de>
2019-03-21 13:45:42 +0100

    wip

---
+++ jane-openapi-configuration.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'openapi-file' => __DIR__ . '/resources/specification/openapi.json',
+    'namespace' => 'Starweb\Sdk',
+    'directory' => __DIR__ . '/src/Sdk',
+    'reference' => true
+];
